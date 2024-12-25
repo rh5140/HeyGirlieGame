e8f935afc670f1f1f227060b1200a6a52d18d49f
maureen <maureen.zajic4@gmail.com>
2024-12-25 15:59:20 -0600

    basic save/load functionality
    saves data (scene name, week, datesThisWeek, and LIs' datecounts and points) to a json and loads it in when the "play" button from the main menu is pressed

--- HeyGirlie/Assets/Scripts/MainMenu.cs
+++ HeyGirlie/Assets/Scripts/MainMenu.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Collections.Generic;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Intro");
+        string json = File.ReadAllText(Application.persistentDataPath + "/GirlieData.json");
+        PlayerData data = JsonUtility.FromJson<PlayerData>(json);
+        SceneManager.LoadScene(data.getScene());
     }
 }
