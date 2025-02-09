4fc8f1188da8130051b3264bf8cd77b3cbf215a7
maureen <maureen.zajic4@gmail.com>
2025-02-08 19:53:14 -0500

    finished ui implementation

--- HeyGirlie/Assets/Scripts/SaveProfiles.cs
+++ HeyGirlie/Assets/Scripts/SaveProfiles.cs
@@ -11,7 +11,7 @@ public class SaveProfiles : MonoBehaviour
     [SerializeField] private GameObject[] saves;
     [SerializeField] private Sprite defaultScreenshot;
 
-    private int selectedSave;
+    private int selectedSave = 0;
     void Start(){
         loadButton.interactable = false;
         deleteButton.interactable = false;
@@ -42,6 +42,7 @@ public class SaveProfiles : MonoBehaviour
     }
 
     public void DeleteSave(){
+        if(selectedSave < 1 || selectedSave > 10) return;
         SaveManager.DeleteData(selectedSave);
         SetScreenshots();
         SetNames();
