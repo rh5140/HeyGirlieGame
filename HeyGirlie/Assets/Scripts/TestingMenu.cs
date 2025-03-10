a49ea06e8f9ae979ff1f4c3f76756534055d9a5c
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2025-03-09 18:52:43 -0700

    Add special events to testing menu

--- HeyGirlie/Assets/Scripts/TestingMenu.cs
+++ HeyGirlie/Assets/Scripts/TestingMenu.cs
@@ -11,6 +11,7 @@ public class TestingMenu : MonoBehaviour
     [SerializeField] private GameObject menuButton;
     [SerializeField] private TMP_Dropdown weekDropdown;
     [SerializeField] private TMP_Dropdown liDropdown;
+    [SerializeField] private TMP_Dropdown eventDropdown;
   
       public void Open(){
         menu.SetActive(true);
@@ -40,6 +41,48 @@ public class TestingMenu : MonoBehaviour
 
         // if(SaveManager.NewData(null) != null)
             SceneManager.LoadScene(loveInterest+"Date"+week);
+        // else MainMenu.openMaxProfilePopup();
+    }
+
+    public void Event()
+    {
+        string eventName = eventDropdown.options[eventDropdown.value].text;
+        string sceneName = "";
+        int week = 0;
+        switch (eventName)
+        {
+            case "Museum (after Week 2)":
+                sceneName = "Week2Event";
+                week = 3;
+                break;
+            case "Music Festival (after Week 4)":
+                sceneName = "Week4Event";
+                week = 5;
+                break;
+            case "Activities Fair (after Week 6)":
+                sceneName = "Week6Event";
+                week = 7;
+                break;
+            default:
+                sceneName = "SpringFling";
+                week = 9;
+                break;
+        }
+
+        List<LoveInterest> liQueue = GameManager.Instance.priorityQueue();
+        foreach(LoveInterest li in liQueue) if(li != null) Debug.Log(li.GetCharacter());
+
+        PlayerData data = new PlayerData("Kristen", "Spyre", sceneName, week, 0, GameManager.Instance.priorityQueue(),
+                                            GameManager.Instance.schoolDates, GameManager.Instance.elmvilleDates, GameManager.Instance.mordredDates,
+                                            GameManager.Instance.outdoorsDates, GameManager.Instance.awayDates);
+
+        int newProfileNum = SaveManager.getCount();
+        // Debug.Log("newProfileNum = " + newProfileNum);
+        if(newProfileNum <= 10)
+            SaveManager.SaveData(data, newProfileNum);
+
+        // if(SaveManager.NewData(null) != null)
+            SceneManager.LoadScene(sceneName);
         // else MainMenu.openMaxProfilePopup();
     }
 }
