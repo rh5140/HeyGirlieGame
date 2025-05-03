71c3dce7f4693539ca1a7f44d3210c1b87cfb844
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2025-05-02 20:26:33 -0700

    Toggle off testing UI, polyam test buttons for week 6

--- HeyGirlie/Assets/Scripts/TestingButtons/PointsDisplay.cs
+++ HeyGirlie/Assets/Scripts/TestingButtons/PointsDisplay.cs
@@ -6,17 +6,25 @@ public class PointsDisplay : MonoBehaviour
 {
     public TextMeshProUGUI displayText;
     public GameObject panel;
+    public GameObject endDateButton;
+    public GameObject cassButton;
+    GameObject testMenuButton;
     bool panelActive = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         UpdatePoints(0, null);
+        // getting a little gross with it...
+        testMenuButton = GameObject.Find("TestMenuPrefab/Open Button");
     }
 
     public void TogglePanel()
     {
         panelActive = !panelActive;
         panel.SetActive(panelActive);
+        endDateButton.SetActive(panelActive);
+        cassButton.SetActive(panelActive);
+        if (testMenuButton) testMenuButton.SetActive(panelActive);
     }
 
     public void UpdatePoints(int date_points, LoveInterest character)
